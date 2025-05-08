5de58dc5d581aefee21deff4b3074d0cee9b74f9
tmsblnt <balint200440@gmail.com>
2025-05-08 21:19:00 +0200

    Vegelegesites

--- vizsgaremek/index.php
+++ vizsgaremek/index.php
@@ -152,7 +152,7 @@
                                 'id' => 'doc4',
                                 'title' => 'Teljes Dokumentáció',
                                 'description' => '',
-                                'file' => ''
+                                'file' => 'VizsgaDokumentacio.pdf'
                             ]
                         ];
 
@@ -279,7 +279,7 @@
                                             </div>
                                             <div class="gantt-row-bars">
                                                 <div class="gantt-bar" style="width: 15%; left: 0%;">
-                                                    <div class="gantt-bar-label">12</div>
+                                                    <div class="gantt-bar-label">12 nap</div>
                                                     <div class="gantt-tooltip">
                                                         <div class="tooltip-title">Projekt Indítása</div>
                                                         <div class="tooltip-dates">2024.12.25 - 2025.01.06</div>
@@ -389,7 +389,7 @@
                                     <div class="milestone-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 8h1a4 4 0 0 1 0 8h-1"></path><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path><line x1="6" y1="1" x2="6" y2="4"></line><line x1="10" y1="1" x2="10" y2="4"></line><line x1="14" y1="1" x2="14" y2="4"></line></svg>
                                     </div>
-                                    <div class="milestone-date">2024.11.01</div>
+                                    <div class="milestone-date">2024.12.25</div>
                                     <h4 class="milestone-title">Csapat kialakítva</h4>
                                     <p class="milestone-desc">A projekt csapat összeállítása és a szerepkörök kiosztása megtörtént.</p>
                                 </div>
@@ -398,7 +398,7 @@
                                     <div class="milestone-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
                                     </div>
-                                    <div class="milestone-date">2025.01.17</div>
+                                    <div class="milestone-date">2025.03.08</div>
                                     <h4 class="milestone-title">Tervek elfogadva</h4>
                                     <p class="milestone-desc">A projekt tervek és követelmények elfogadásra kerültek, a fejlesztés kezdődhet.</p>
                                 </div>
@@ -407,7 +407,7 @@
                                     <div class="milestone-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
                                     </div>
-                                    <div class="milestone-date">2024.01.15</div>
+                                    <div class="milestone-date">2025.04.07</div>
                                     <h4 class="milestone-title">Tesztelés befejezve</h4>
                                     <p class="milestone-desc">A rendszer tesztelése sikeresen lezárult, a kritikus hibák javításra kerültek.</p>
                                 </div>
@@ -416,7 +416,7 @@
                                     <div class="milestone-icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
                                     </div>
-                                    <div class="milestone-date">2024.03.15</div>
+                                    <div class="milestone-date">2024.05.08</div>
                                     <h4 class="milestone-title">Projekt átadva</h4>
                                     <p class="milestone-desc">A projekt sikeresen lezárult, a teljes dokumentáció és a rendszer átadásra került.</p>
                                 </div>
